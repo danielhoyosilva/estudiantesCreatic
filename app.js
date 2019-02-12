432923edd09409268ca38239f2fca51d9e0e59fc
Daniel Hoyos <danielhoyos@localhost.localdomain>
2019-02-12 18:49:52 -0500

    Proyecto Estudiantes Creatic

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require("mysql");
+
+// Conexión Base de Datos
+const con = mysql.createConnection({
+   host: 'localhost',
+   user: 'danielhoyos',
+   password: 'servercentos',
+   database: 'creatic'
+});
+
+con.connect(error => {
+   if(error){
+     console.log('Error al conectarse a la base de datos');
+     return;
+   }
+
+   console.log('Conexion exitosa');
+});
+
+
+
+// Codigo
+
+var app = express();
+
+app.get('/', function (req, res){
+   
+   let query = "SELECT * FROM estudiantes;";
+   
+   con.query(query, (error, result) => {
+      if (error){
+         console.log("Error en la consulta " + error);
+      }
+      
+      res.json({ code: 200, response: result});
+      
+      console.log(result);
+   });
+   
+   con.end(error => {});
+});
+
+app.listen(3000, function(){
+   console.log('Esta app esta corriendo!');
+});
+
